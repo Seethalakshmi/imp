10c6bea18579a9f3c69e1070fa6316d458002eb4
mfortenberry <mfortenberry@yorksolutions.net>
2022-04-26 09:37:56 -0500

    use constants instead of duping strings

--- src/App.test.js
+++ src/App.test.js
@@ -12,17 +12,18 @@ import {App} from './App';
 // Is this a unit test? no, it is a units test
 test('should display the counter component when logged in', () => {
   let _memos = undefined
+  const expectedText = 'This is the expected text'
 
   const mock = ({memos}) => {
     _memos = memos
-    return <>This is the expected count</>
+    return <>{expectedText}</>
   }
 
   const noText = 'NONONO'
   const nonono = () => <>{noText}</>
 
   render(<App loggedInInit={true} _Memos={mock} _Login={nonono}/>);
-  const h1 = screen.getByText(/This is the expected count/)
+  const h1 = screen.getByText(expectedText)
   expect(h1).toBeInTheDocument()
   expect(_memos).toBeDefined()
   expect(screen.queryByText(noText)).not.toBeInTheDocument()
@@ -30,11 +31,12 @@ test('should display the counter component when logged in', () => {
 
 // This is a unit test
 test('should display the login screen when logged out', () => {
-  const mockLogin = () => <>This is the expected text</>
+  const expectedText = 'This is the expected text'
+  const mockLogin = () => <>{expectedText}</>
   const noText = 'NONONO'
   const nonono = () => <>{noText}</>
   render(<App _Login={mockLogin} _Memos={nonono}/>);
-  const element = screen.getByText(/This is the expected text/);
+  const element = screen.getByText(expectedText);
   expect(element).toBeInTheDocument();
   expect(screen.queryByText(noText)).not.toBeInTheDocument()
 });
