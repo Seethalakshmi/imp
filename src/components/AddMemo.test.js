950c0aba4bdbb2bafb23b347ea4f03d769d435c5
mfortenberry <mfortenberry@yorksolutions.net>
2022-04-29 16:23:11 -0500

    test AddMemo

--- src/components/AddMemo.test.js
+++ src/components/AddMemo.test.js
@@ -1,5 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import AddMemo from "./AddMemo";
+import userEvent from "@testing-library/user-event";
+import {ADD_MEMO} from "../modules/memos";
 
 test('should show input fields for each key in memo', () => {
     render(<AddMemo/>)
@@ -9,4 +11,28 @@ test('should show input fields for each key in memo', () => {
     expect(screen.getByRole('checkbox')).toBeInTheDocument()
     expect(screen.getByText('Cancel')).toBeInTheDocument()
     expect(screen.getByText('Add')).toBeInTheDocument()
+})
+
+it('should dispatch an ADD_MEMO action w/ a memo payload when the Add button is clicked', () => {
+    const mock = jest.fn()
+    const useDispatch = () => mock
+
+    render(<AddMemo _useDispatch={useDispatch}/>)
+
+    const memo = {
+        title: 'title',
+        description: 'desc',
+        date: new Date(),
+        complete: true
+    }
+
+    memo.date.setTime(0)
+
+    userEvent.type(screen.getByPlaceholderText('Title'), memo.title)
+    userEvent.type(screen.getByPlaceholderText('Description'), memo.description)
+    userEvent.type(screen.getByPlaceholderText('Date'), memo.date.toISOString().substring(0,10))
+    userEvent.click(screen.getByRole('checkbox'))
+    userEvent.click(screen.getByText('Add'))
+
+    expect(mock).toHaveBeenCalledWith({type: ADD_MEMO, memo})
 })
